48126b60c5bdbd81b3646152903bb58125f7d2ef
= <=>
2021-12-02 11:57:02 -0600

    updated thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,60 @@
-const {Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+//call our date format
+const dateFormat = require('../utils/dateFormat');
 
-const ThoughtSchema = new Schema (
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    });
+
+const ThoughtSchema = new Schema(
     {
         thoughtText: {
-            type:String,
-            required:true,
+            type: String,
+            required: true,
             minlength: 1,
             maxlength: 280
         },
         createdAt: {
-            type:Date,
+            type: Date,
             default: Date.now,
             //gets our dateFormat 
-            get:(createdAtVal) => dateFormat(createdAtVal)
-
-        }
-
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
-            virtuals:true,
-            getters:true
+            virtuals: true,
+            getters: true
         },
-        id:false
+        id: false
     })
 
 const Thought = model('Thought', ThoughtSchema);
