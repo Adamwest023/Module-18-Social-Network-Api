e95e7ed5787c3770002908f25898d79666ddfe1d
= <=>
2021-12-06 11:57:57 -0600

    wireframe for routes

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
-
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const {User, Thought} = require('./models');
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Notedb', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
+
+//route to get all users
+app.get('/users',(req.res))
+
+// route to get user by id 
+
+// route to add a user
+
+// route to update a user
+
+// route to delete a user
+
+//route to add a new friend
+
+// route to delete a friend 
+
+//
